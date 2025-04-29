91716de9e1c71c5d42489a5c1a211fd888a801b7
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-29 21:11:07 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { QuestionsAndAnswers } from "home/QuestionsAndAnswers";
 export default function Home() {
   return (
     <motion.main
-      className="mx-auto max-w-screen-2xl bg-dot px-8 pb-32 text-gray-900 lg:px-12"
+      className="mx-auto max-w-screen-2xl bg-[var(--theme-dark-blue)] px-8 pb-32 text-[var(--theme-gray)] lg:px-12"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
