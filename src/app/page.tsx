7d6a4340646cb2311388886622721d9fa7802d35
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-29 13:50:05 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import { Hero } from "home/Hero";
 import { Steps } from "home/Steps";
 import { Features } from "home/Features";
@@ -7,41 +6,12 @@ import { QuestionsAndAnswers } from "home/QuestionsAndAnswers";
 
 export default function Home() {
   return (
-    <motion.main
-      className="mx-auto max-w-screen-2xl bg-dot px-6 py-12 text-gray-900 lg:px-16 lg:py-20"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
+    <main className="mx-auto max-w-screen-2xl bg-dot px-8 pb-32 text-gray-900 lg:px-12">
       <Hero />
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.2, duration: 0.5 }}
-      >
-        <Steps />
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.4, duration: 0.5 }}
-      >
-        <Features />
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.6, duration: 0.5 }}
-      >
-        <Testimonials />
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.8, duration: 0.5 }}
-      >
-        <QuestionsAndAnswers />
-      </motion.div>
-    </motion.main>
+      <Steps />
+      <Features />
+      <Testimonials />
+      <QuestionsAndAnswers />
+    </main>
   );
 }
