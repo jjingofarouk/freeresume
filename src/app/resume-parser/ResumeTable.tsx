7af56fcfd588b2516173c64c9170343c4400132a
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-11 19:25:44 +0300

    Update ResumeTable.tsx

--- src/app/resume-parser/ResumeTable.tsx
+++ src/app/resume-parser/ResumeTable.tsx
@@ -2,10 +2,10 @@ import { Fragment } from "react";
 import type { Resume } from "lib/redux/types";
 import { initialEducation, initialWorkExperience } from "lib/redux/resumeSlice";
 import { deepClone } from "lib/deep-clone";
-import styles from "./ResumeTable.css";
+import "./ResumeTable.css";
 
 const TableRowHeader = ({ children }: { children: React.ReactNode }) => (
-  <tr className={styles.tableRowHeader}>
+  <tr className="tableRowHeader">
     <th scope="colgroup" colSpan={2}>
       {children}
     </th>
@@ -21,14 +21,14 @@ const TableRow = ({
   value: string | string[];
   className?: string;
 }) => (
-  <tr className={`${styles.tableRow} ${className || ""}`}>
+  <tr className={`tableRow ${className || ""}`}>
     <th scope="row">{label}</th>
     <td>
       {typeof value === "string" ? (
         value
       ) : (
         value.map((x, idx) => (
-          <div key={idx} className={styles.listItem}>
+          <div key={idx} className="listItem">
             {x}
           </div>
         ))
@@ -57,9 +57,9 @@ export const ResumeTable = ({ resume }: { resume: Resume }) => {
   }
 
   return (
-    <div className={styles.tableContainer}>
-      <table className={styles.table}>
-        <tbody className={styles.tableBody}>
+    <div className="tableContainer">
+      <table className="table">
+        <tbody className="tableBody">
           <TableRowHeader>Candidate Profile</TableRowHeader>
           <TableRow label="Full Name" value={resume.profile.name} />
           <TableRow label="Email Address" value={resume.profile.email} />
@@ -79,7 +79,7 @@ export const ResumeTable = ({ resume }: { resume: Resume }) => {
                 value={education.descriptions}
                 className={
                   educations.length > 1 && idx !== educations.length - 1
-                    ? styles.tableRowDivider
+                    ? "tableRowDivider"
                     : ""
                 }
               />
@@ -96,7 +96,7 @@ export const ResumeTable = ({ resume }: { resume: Resume }) => {
                 value={workExperience.descriptions}
                 className={
                   workExperiences.length > 1 && idx !== workExperiences.length - 1
-                    ? styles.tableRowDivider
+                    ? "tableRowDivider"
                     : ""
                 }
               />
@@ -114,7 +114,7 @@ export const ResumeTable = ({ resume }: { resume: Resume }) => {
                 value={project.descriptions}
                 className={
                   resume.projects.length > 1 && idx !== resume.projects.length - 1
-                    ? styles.tableRowDivider
+                    ? "tableRowDivider"
                     : ""
                 }
               />
