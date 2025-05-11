1d31703f1fd55c42ac651ccff9d5e405a7b28be1
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-11 20:26:14 +0300

    Update globals-css.ts

--- src/app/globals-css.ts
+++ src/app/globals-css.ts
@@ -1,5 +1,16 @@
+// styles/global.css.ts
 export const CSS_VARIABLES = {
   "--top-nav-bar-height": "3.5rem",
   "--resume-control-bar-height": "3rem",
   "--resume-padding": "1.5rem",
+  "--theme-dark-teal": "#1a535c",
+  "--theme-teal": "#4a8e96",
+  "--theme-teal-dark": "#163f45",
+  "--theme-light-gray": "#e5e7eb",
+  "--theme-black": "#1f2937",
+  "--spacing-xs": "8px",
+  "--spacing-sm": "12px",
+  "--spacing-md": "16px",
+  "--spacing-lg": "24px",
+  "--spacing-xl": "32px",
 } as const;
