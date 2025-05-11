348078d115d5b785db5c3e5145753c04bfb1e7e6
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-11 20:27:16 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/app/**/*.{ts,tsx,mdx}"],
   theme: {
     extend: {
+      colors: {
+        "theme-dark-teal": "var(--theme-dark-teal)",
+        "theme-teal": "var(--theme-teal)",
+        "theme-teal-dark": "var(--theme-teal-dark)",
+        "theme-light-gray": "var(--theme-light-gray)",
+        "theme-black": "var(--theme-black)",
+      },
+      spacing: {
+        xs: "var(--spacing-xs)",
+        sm: "var(--spacing-sm)",
+        md: "var(--spacing-md)",
+        lg: "var(--spacing-lg)",
+        xl: "var(--spacing-xl)",
+      },
+      borderRadius: {
+        xl: "1rem",
+      },
       backgroundImage: {
         dot: "url('/assets/dots.svg')",
       },
